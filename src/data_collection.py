138328a529a107ff8c8d7936e0ea0810db48b9a3
sophiamjiali <sophiamjiali@gmail.com>
2024-01-06 15:08:31 -0500

    Adjusted files for better readability

--- src/data_collection.py
+++ src/data_collection.py
@@ -10,8 +10,10 @@ ENDPOINT = 'chart'
 # serialize ticker list into a separated string of tickers
 def serialize_ticker_list():
     ticker_string = ''
+
     for ticker in TICKERS:  # loop through each element and add
         ticker_string += (ticker + ',')
+
     return ticker_string[:-1]  # drop last added comma
 
 
@@ -23,24 +25,31 @@ def select_endpoint(tickers):
 # pull closing price time series for each stock
 def pull_price_time_series(tickers, bank_data):
     bank_price_series = {}  # initialize the dictionary
+
     for ticker in tickers:
         bank_price_series.update({ticker: pd.DataFrame(bank_data[ticker][ENDPOINT])['close']})
+
     return bank_price_series
 
 
 # formats DataFrame with date as index and closing price as column
 def fetch_bank_stock_data(bank_data):
     stock_data = []  # initialize the list
+
     for ticker in TICKERS:
         stock_data.append(pd.DataFrame(bank_data[ticker][ENDPOINT])['close'])
+
     # add the first bank of TICKERS
     stock_data.append(pd.DataFrame(bank_data[TICKERS[0]][ENDPOINT])['date'])
+
     # format columns, names, and data
     column_names = TICKERS.copy()
     column_names.append('Date')
+
     # mutate bank_data
     bank_data = pd.concat(stock_data, axis=1)
     bank_data.columns = column_names
     bank_data.set_index('Date', inplace=True)
+
     # return the formatted DataFrame; bank_data is mutated in-place
     return stock_data
--- src/visualizer.py
+++ src/visualizer.py
@@ -3,5 +3,9 @@ import matplotlib.pyplot as plt
 
 # function to be called in the main file to put together the boxplot
 def create_boxplot(bank_data):
+
     # create a boxplot along columns, not rows; transpose DataFrame
     plt.boxplot(bank_data.transpose)
+
+    # styling
+    plt.title('Boxplot of Bank Stock Prices (5Y Lookback)', fontsize=20)  # chart title
