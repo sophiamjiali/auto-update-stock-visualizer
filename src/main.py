b6091c5feb155b70213b1168818d8e5d80c624a9
sophiamjiali <sophiamjiali@gmail.com>
2024-01-10 16:45:50 -0500

    Issues with pandas read_jason certificate handshake

--- src/main.py
+++ src/main.py
@@ -14,9 +14,9 @@ price_series = pull_price_time_series(tickers, bank_data)
 # format bank_data and fetch stock data
 stock_data = fetch_bank_stock_data(bank_data)
 
+
 # from visualizer.py; visualize collected data
 
 # generate boxplot
 create_boxplot(bank_data)
 # generate scatterplot
-
