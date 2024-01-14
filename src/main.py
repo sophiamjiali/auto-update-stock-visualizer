b7545336c14fd78ae59de899625f9f7cbec0b21e
sophiamjiali <sophiamjiali@gmail.com>
2024-01-14 01:16:19 -0500

    Stashing

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 from data_collection import *
 from visualizer import *
+import requests
 
 # from data_collection.py; set up data to be visualized
 
 # serialize the ticker list into a string
 tickers = serialize_ticker_list()
+
 # format the http request
 HTTP_request = select_endpoint(tickers)
+
 # fetch bank data
-bank_data = pd.read_json(HTTP_request)
+response = requests.get(HTTP_request, verify=False).json()
+bank_data = pd.DataFrame(response)
+
 # pull closing prices
-price_series = pull_price_time_series(tickers, bank_data)
+price_series = pull_price_time_series(TICKERS, bank_data)
+
 # format bank_data and fetch stock data
 stock_data = fetch_bank_stock_data(bank_data)
 
